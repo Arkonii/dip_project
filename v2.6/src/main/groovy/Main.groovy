787c4789312711eb9cf9ed1dedaa53fb714af6ca
Arkonii <arek199922@gmail.com>
2024-01-15 07:15:26 +0100

    v1 v2 v3

---
+++ v2.6/src/main/groovy/Main.groovy
@@ -0,0 +1,25 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+class Main {
+  static void main(String[] args) {
+    HttpClient httpClient = HttpClient.newHttpClient();
+
+
+    HttpRequest httpRequest = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:1080/example"))
+            .GET()
+            .build();
+
+    try {
+      HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+      System.out.println("Status: " + httpResponse.statusCode());
+      System.out.println("Treść odpowiedzi: " + httpResponse.body());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ v3/src/main/groovy/Main.groovy
@@ -0,0 +1,32 @@
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+
+class Main extends Application {
+    @Override
+    void start(Stage primaryStage) {
+        primaryStage.title = "GroovyFX Example"
+
+        def button = new Button("Click me!")
+        button.onAction = { event ->
+            println("Button clicked!")
+        }
+
+        def root = new StackPane()
+        root.children << button
+
+        primaryStage.scene = new Scene(root, 300, 250)
+
+        primaryStage.show()
+    }
+
+    static void main(String[] args) {
+        // Wskazanie ścieżki do bibliotek JavaFX
+        System.setProperty("javafx.sdk.path", "C:\\Program Files\\Eclipse Adoptium\\javafx-sdk-20.0.2")
+
+        // Uruchomienie aplikacji JavaFX
+        launch(args)
+    }
+}
